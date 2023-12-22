b8a4fbe1a5878a2412bd8ca628f0773d0226238d
Joshua Spergel <44729767+spergel@users.noreply.github.com>
2023-12-22 10:35:10 -0500

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -2,15 +2,12 @@ import openai
 #import { ReplicateStream, StreamingTextResponse } from "ai";
 export const runtime = "edge";
 
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN,
-});
+client = openai.OpenAI(
+    base_url = "https://api.endpoints.anyscale.com/v1",
+    api_key = 'esecret_2fiq3yggj78a759vxrl88zp62n'
+)
 
-if (!process.env.REPLICATE_API_TOKEN) {
-  throw new Error(
-    "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
-  );
-}
+
 
 export async function POST(req) {
   const params = await req.json();
@@ -24,9 +21,10 @@ export async function POST(req) {
 }
 
 async function runMixtral({ model, prompt, maxTokens, temperature, topP }) {
-  response = openai.Completion.create(
+  response = client.chat.completions.create(
       engine="mistralai/Mixtral-8x7B-Instruct-v0.1",
-      prompt=`${prompt}`,
+      messages=[{"role": "system", "content": "You are a helpful assistant."}, 
+              {"role": "user", "content": prompt],
       max_tokens=maxTokens,  # Adjust max tokens as needed
       temperature=temperature, # Adjust temperature as needed
       stream = true
